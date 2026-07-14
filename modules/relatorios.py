a7b88df56a3c1b9751bf2b92cc06ec9ea61639ff
Daniel <78493199+danielan09@users.noreply.github.com>
2026-07-13 21:03:55 -0300

    Update relatorios.py

--- modules/relatorios.py
+++ modules/relatorios.py
@@ -1,7 +1,7 @@
 from modules.gerenciadorLivros import listar_livros
 from usuarios import listar_alunos
 from modules.emprestimos import realizar_emprestimo
-from modules.aluno import Aluno
+from models.aluno import Aluno
  
 class Relatorio:
     def __init__(self, listar_livros, listar_alunos, realizar_emprestimo):
