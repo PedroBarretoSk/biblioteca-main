d61ba0f9488a46a3a35e640da17273d852c585a2
marcelo-alan <marceloalan555@gmail.com>
2026-07-14 20:03:47 -0300

    transformar dados em json

--- models/Livro.py
+++ models/Livro.py
@@ -9,3 +9,19 @@ class Livro:
     def exibir_detalhes(self):
         status = "Disponível" if self.disponivel else "Indisponível"
         print(f"[{self.codigo}] {self.titulo} - Autor: {self.autor} | Cat: {self.categoria} | Status: {status}")
+
+
+    def transformar_dict(self):
+        return {
+            "codigo": self.codigo,
+            "titulo": self.titulo,
+            "autor": self.autor,
+            "categoria": self.categoria,
+            "disponivel": self.disponivel
+        }
+
+    @staticmethod
+    def from_dict(dados: dict):
+        livro = Livro(dados["codigo"], dados["titulo"], dados["autor"], dados["categoria"])
+        livro.disponivel = dados.get("disponivel", True)
+        return livro
